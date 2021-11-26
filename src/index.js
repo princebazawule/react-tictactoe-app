dca32a5e6e566aba30721cc612d95e4687a07238
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-25 21:04:01 -0400

    feat: add travis yaml

--- src/index.js
+++ src/index.js
@@ -138,18 +138,26 @@ const Board = (props) => {
         }
 
         return (
+          <>
+              <div>
+              <h1>TicTacToe!</h1>
+              <ul />
+              <br />
+            </div>
+
             <div className="game">
-            <div className="game-board">
-            <Board
-                squares={current.squares}
-                onClick={(i) => this.handleClick(i)}
-            />
+              <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                />
+              </div>
+              <div className="game-info">
+                  <div>{status}</div>
+                  <ol>{moves}</ol>
+              </div>
             </div>
-            <div className="game-info">
-                <div>{status}</div>
-                <ol>{moves}</ol>
-            </div>
-            </div>
+          </>
         );
     }
   }
